43c2d37fd88b596333f99e75a0d2f51d810c79b3
David <dph0718@gmail.com>
2019-02-18 16:57:31 -0700

    initial commit

---
+++ MyClass.java
@@ -0,0 +1,9 @@
+
+// the class must match the file name "MyClass"
+public class MyClass {
+    // every class gotta have a "main" function.
+  public static void main(String[] args) {
+    //   Eeerrry code statement gotta end with ;
+    System.out.println("Hello World");
+  }
+}
